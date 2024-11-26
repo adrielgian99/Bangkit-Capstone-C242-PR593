d54370fded250b0582bee7659c6db107740e4344
chrizzzii <clvtito@gmail.com>
2024-11-26 19:34:31 +0700

    api specs

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const apiDocs = require("./api-docs.json");
-const userRoutes = require("./routes/userRoutes");
+const cors = require("cors"); // Impor CORS
 
 const app = express();
 const PORT = 3000;
 
-// Middleware
+// Middleware untuk CORS
+app.use(cors({
+    origin: 'http://localhost:3000', // Ganti dengan URL front-end Anda
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metode HTTP yang diizinkan
+    allowedHeaders: ['Content-Type', 'Authorization'] // Header yang diizinkan
+}));
+
+// Middleware lainnya
 app.use(express.json());
 
 // Swagger API Documentation
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocs));
 
-// Routes
-app.use("/api/user", userRoutes);
 
 // Server
 app.listen(PORT, () => {
